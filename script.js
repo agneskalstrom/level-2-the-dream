e300be63c4653f91e7a8545b5da3450025b1db93
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2021-05-27 22:43:24 +0200

    add all files

---
+++ script.js
@@ -0,0 +1,44 @@
+const postcardWrapper = document.querySelector(".postcard");
+const postcard = document.querySelector(".postcard-inner");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modal = document.querySelector(".modal");
+const wake = document.querySelector("#wake");
+const closeModal = document.querySelector("#cancel");
+const player = document.querySelector("[camera]");
+const word1 = "apple";
+const word2 = "rabies";
+const word3 = "bay";
+const ambience = new Audio("/assets/sounds/ambience.wav");
+
+player.addEventListener("collide", function (e) {
+  let id = e.detail.body.el.getAttribute("id");
+  if (id === "door") {
+    modalWrapper.style.display = "flex";
+    wake.addEventListener("click", function () {
+      const input1 = document.querySelector("#word-1").value;
+      const input2 = document.querySelector("#word-2").value;
+      const input3 = document.querySelector("#word-3").value;
+      if (input1 == word1 && input2 == word2 && input3 == word3) {
+        document.body.style.opacity = "0";
+        ambience.pause();
+      } else {
+        modal.classList.add("wobble");
+        setTimeout(function () {
+          modal.classList.remove("wobble");
+        }, 1000);
+      }
+    });
+    closeModal.addEventListener("click", function () {
+      modalWrapper.style.display = "none";
+    });
+  }
+});
+
+postcard.addEventListener("click", function () {
+  postcardWrapper.style.opacity = "0";
+  setTimeout(function () {
+    postcardWrapper.remove();
+  }, 1000);
+  ambience.play();
+  ambience.loop = true;
+});
